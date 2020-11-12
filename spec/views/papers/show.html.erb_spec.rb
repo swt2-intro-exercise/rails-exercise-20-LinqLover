b1553cf672e59308197548b1a341226321bee9e9
Christoph Thiede <christoph.thiede@student.hpi.de>
2020-11-12 09:48:46 +0100

    Show authors of paper. Closes #8

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -3,9 +3,10 @@ require 'rails_helper'
 RSpec.describe "papers/show", type: :view do
   before(:each) do
     @paper = assign(:paper, Paper.create!(
-      :title => "Title",
-      :venue => "Venue",
-      :year => 2
+      title: "Title",
+      venue: "Venue",
+      year: 2,
+      authors: [build(:author)]
     ))
   end
 
@@ -14,5 +15,6 @@ RSpec.describe "papers/show", type: :view do
     expect(rendered).to match(/Title/)
     expect(rendered).to match(/Venue/)
     expect(rendered).to match(/2/)
+    expect(rendered).to match(/Turing/)
   end
 end
